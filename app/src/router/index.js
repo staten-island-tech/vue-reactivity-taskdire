7a391700442b524c5065b4bf1659b1ce923d3078
taskdire <136046239+taskdire@users.noreply.github.com>
2025-03-24 10:58:58 -0400

    Update index.js

--- app/src/router/index.js
+++ app/src/router/index.js
@@ -1,13 +1,14 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import CardsData from '@/views/CardsData.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
-      name: 'home',
-      component: HomeView,
+      path: '/cards',
+      name: 'cards',
+      component: CardsData,
     },
     {
       path: '/about',
